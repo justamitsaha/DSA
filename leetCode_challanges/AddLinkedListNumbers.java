9665c8171c4722b1181dc7258a9300db1ec7ea70
Amit Saha <justamitsaha@gmail.com>
2026-07-08 07:51:54 +0530

    leet code problems

--- leetCode_challanges/AddLinkedListNumbers.java
+++ leetCode_challanges/AddLinkedListNumbers.java
@@ -1,17 +1,22 @@
-void main() {
-    ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3, null)));
-    ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4, null)));
-    Solution solution = new Solution();
-    ListNode result = solution.addTwoNumbers(l1, l2);
-    while (result != null) {
-        System.out.print(result.val + " ");
-        result = result.next;
+public class AddLinkedListNumbers {
+    public static void main(String[] args) {
+        //l1 =[9]
+        //l2 = [1,9,9,9,9,9,9,9,9,9]
+
+        ListNode l1 = new ListNode(9, null);
+        ListNode l2 = new ListNode(1, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, null)))))))));
+        Solution solution = new Solution();
+        ListNode result = solution.addTwoNumbers(l1, l2);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
     }
 }
 
 
 //      Definition for singly-linked list.
-public class ListNode {
+class ListNode {
     int val;
     ListNode next;
 
@@ -41,37 +46,41 @@ public class ListNode {
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        int val1 =0;
-        int val2 = 0;
+        long val1 = 0;
+        long val2 = 0;
         int count = 0;
         boolean bool1 = false;
         boolean bool2 = false;
-        while(true){
-            if(null != l1){
-                val1 = val1 + l1.val*(Math.powExact(10,count));
+        while (true) {
+            if (null != l1) {
+                val1 = val1 + l1.val * (Math.powExact(10, count));
                 l1 = l1.next;
-            }else {
+            } else {
                 bool1 = true;
             }
 
-            if(null != l2){
-                val2 = val2 + l2.val*(Math.powExact(10,count));
+            if (null != l2) {
+                val2 = val2 + l2.val * (Math.powExact(10, count));
                 l2 = l2.next;
-            }else {
+            } else {
                 bool2 = true;
             }
             count++;
-            if(bool1 && bool2)
+            if (bool1 && bool2)
                 break;
         }
+        System.out.println(val1+val2);
 
         String addition = String.valueOf(val1 + val2);
         ListNode sum = new ListNode();
-        for (int i = addition.length() - 1; i >= 0; i--) {
+        for (int i = 0; i < addition.length(); i++) {
             char c = addition.charAt(i);
-            sum = new ListNode(Character.getNumericValue(c), sum);
+            if (i == 0) {
+                sum = new ListNode(Character.getNumericValue(c));
+            } else {
+                sum = new ListNode(Character.getNumericValue(c), sum);
+            }
         }
-
 
         return sum;
     }
